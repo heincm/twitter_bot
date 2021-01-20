02dd6bf0b7276605b8e57220d059e543801fd92d
Chris <chris.m.hein@gmail.com>
2021-01-19 22:51:51 -0500

    search for new tweets just before the hour and like them.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import requests
 import tweepy as tp
 import constants
+import datetime
 
 consumer_key = constants.consumer_key
 consumer_secret = constants.consumer_secret
@@ -34,7 +35,7 @@ def follow_back(follower):
 def look_for_mentions():
     for mention in api.mentions_timeline():
         try:
-            if mention.author.following == True:
+            if mention.author.following:
                 return
             else:
                 api.create_friendship(mention.author.id)
@@ -53,8 +54,37 @@ def call_api():
     gbp_amt = gbp_amt[:-2]
     eur_amt = middle_man["bpi"]["EUR"]["rate"]
     eur_amt = eur_amt[:-2]
-    bitcoin_price = 'Current Bitcoin Price:\nUSD $' + usd_amt + '\nGBP £' + gbp_amt + '\nEuro €' + eur_amt + '\n#bitcoin #btc $btc #btcusd #btcgbp #btceur #crypto #cryptocurrency'
+    bitcoin_price = "Current Bitcoin Price:\nUSD $" + usd_amt + '\nGBP £' + gbp_amt + '\nEuro €' + eur_amt + '\n#bitcoin #btc $btc #btcusd #btcgbp #btceur #crypto #cryptocurrency'
     return bitcoin_price
+
+
+def search_relevant_tweets():
+    tag = ("bitcoin", "btc", "crypto", "cryptocurrency", "#bitcoin", "#btc", "$btc")
+    for tweet in tp.Cursor(api.search, tag, result_type="recent", lang="en").items(10):
+        try:
+            favorite_a_tweet(tweet)
+            print("tweet favorited")
+        except tp.TweepError as e:
+            print(e.reason)
+
+
+def favorite_a_tweet(tweet):
+    #  have to get the use the api method to get the id to see actual
+    #  info. on whether tweet has been favorited or not
+    new_tweet = api.get_status(tweet.id)
+    if (new_tweet.favorited is False) and (tweet.user.id != user.id):
+        return tweet.favorite()
+    return
+
+
+def check_time():
+    now = datetime.datetime.now()
+    two_after_the_hour = now.replace(minute=52)
+    sometime_after_the_hour = now.replace(minute=57)
+
+    if two_after_the_hour < now < sometime_after_the_hour:
+        return search_relevant_tweets()
+    print("out of range")
 
 
 # Press the green button in the gutter to run the script.
@@ -62,3 +92,4 @@ if __name__ == '__main__':
     follow_followers()
     api.update_status(call_api())
     look_for_mentions()
+    check_time()
